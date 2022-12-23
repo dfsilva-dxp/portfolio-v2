85d201e2cde9128aec7ff2d650019df75c7691aa
Daniel Silva <dfsilva.dxp@gmail.com>
2022-12-23 08:29:21 -0300

    chore: configure jest and testing library

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleDirectories: ["node_modules", "src"],
+  testPathIgnorePatterns: ["/node_modules/", "/.next/", "/public/"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/components/index.ts",
+    "!src/pages/**/*.{js,jsx,ts,tsx}",
+    "!src/routes/*.{js,jsx,ts,tsx}",
+    "!src/styles/**/*.ts",
+    "!src/template/**/*.{js,jsx,ts,tsx}",
+    "!src/**/stories.tsx",
+    "!src/@types/**/*.d.ts",
+    "!src/utils/tests/renderWithTheme.tsx",
+    "!src/**/mock.ts",
+    "!src/App.tsx",
+    "!src/main.tsx",
+    "!src/**/*.d.ts"
+  ],
+  setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
+  modulePaths: ["<rootDir>/src", "<rootDir>/.jest"],
+  moduleNameMapper: {
+    "^.+.(svg)$": "jest-transform-stub"
+  }
+};
